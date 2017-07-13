fb64b5693bf02e2306acc5bf879289898d726b3c
Unknown <geoff.w.wilkinson@gmail.com>
2017-07-13 12:50:53 +0700

    Sprite support
    added sprite support in a reasonably generic way

--- space/js/GraphicsCommon.js
+++ space/js/GraphicsCommon.js
@@ -22,3 +22,58 @@ function drawBitmapCenteredAtLocationWithRotation(graphic, atX, atY, withAngle) 
 	canvasContext.drawImage(graphic, -graphic.width/2, -graphic.height/2); // center, draw
 	canvasContext.restore(); // undo the translation movement and rotation since save()
 }
+
+function animatedSprite() {
+	this.init = function(whichImage, horizontalFrames, verticalFrames) {
+		this.image = whichImage;
+		this.hFrames = horizontalFrames;
+		this.vFrames = verticalFrames;
+
+		this.frameWidth = this.image.width / this.hFrames;
+		this.frameHeight = this.image.height / this.vFrames;
+		this.numFrames = this.hFrames * this.vFrames;
+
+		this.sequenceNumber = 0;
+		this.currentFrame = 0;
+		this.currentHFrame = 0;
+		this.currentVFrame = 0;
+		this.cycleByThis = 0;
+	}
+
+	this.setAnimationSequence = function(animationFrames, cycleBehaviour) {
+		this.animationFrames = animationFrames;
+		this.cycleBehaviour = cycleBehaviour;
+		this.cycleByThis = 1;
+	}
+
+	this.tickAnimation = function() {
+		this.sequenceNumber += this.cycleByThis;
+		if(this.sequenceNumber > this.animationFrames.length - 1) {
+			if(this.cycleBehaviour == "loop") {
+				this.sequenceNumber = 0;
+			} else if(this.cycleBehaviour == "reverse") {
+				this.cycleByThis = -this.cycleByThis;
+				this.sequenceNumber += this.cycleByThis;
+			}
+		} else if(this.sequenceNumber < 0) {
+			if(this.cycleBehaviour == "loop") {
+				this.sequenceNumber = this.animationFrames.length - 1;
+			} else if(this.cycleBehaviour == "reverse") {
+				this.cycleByThis = -this.cycleByThis;
+				this.sequenceNumber += this.cycleByThis;
+			}
+
+		}
+		this.currentFrame = this.animationFrames[this.sequenceNumber];
+		this.currentHFrame = this.currentFrame % this.hFrames;
+		this.currentVFrame = Math.floor(this.currentFrame / this.vFrames);
+	}
+
+	this.draw = function(atX, atY, withAngle) {
+		canvasContext.save();
+		canvasContext.translate(atX, atY);
+		canvasContext.rotate(withAngle);
+		canvasContext.drawImage(this.image, this.currentHFrame * this.frameWidth, this.currentVFrame * this.frameHeight, this.frameWidth, this.frameHeight, -this.frameWidth/2, -this.frameHeight/2, this.frameWidth, this.frameHeight);
+		canvasContext.restore();
+	}
+}
