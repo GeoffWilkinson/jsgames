e2b35c9dcf8fa37a535910a54898b5e35aae1ffe
Unknown <geoff.w.wilkinson@gmail.com>
2017-07-14 18:00:52 +0700

    Added cannon as a class type
    Instead of being a property of a ship itself, the cannon is now defined as such, this is a precursor to adding more weapon types

---
+++ space/js/Weapon.js
@@ -0,0 +1,17 @@
+function cannonClass(damage, cooldown, ammoImage, ammoSpeed, ammoLife, ammoCollisionRadius) {
+	this.damage = damage;
+	this.cooldown = 0;
+	this.baseCooldown = cooldown;
+	this.ammoImage = ammoImage;
+	this.ammoSpeed = ammoSpeed;
+	this.ammoLife = ammoLife;
+	this.ammoCollisionRadius = ammoCollisionRadius;
+
+	this.fire = function(fromThis) {
+		var newShot = new shotClass();
+		newShot.init(this.ammoImage, this.ammoCollisionRadius);
+		newShot.shootFrom(fromThis, this.ammoSpeed, this.ammoLife);
+		playerShots.push(newShot);
+		this.cooldown = this.baseCooldown;
+	}
+}
