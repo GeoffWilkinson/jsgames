88902db251f888039659ce6d48f636520bb92655
Unknown <geoff.w.wilkinson@gmail.com>
2017-07-15 01:02:17 +0700

    Missile tuning
    Tuning missile movement and guidance

--- space/js/Missile.js
+++ space/js/Missile.js
@@ -1,6 +1,6 @@
-const MISSILE_THRUST = 6;
-const MISSILE_TURN_RATE = 1;
-const MISSILE_LIFE = 60;
+const MISSILE_THRUST = 0.15;
+const MISSILE_TURN_RATE = 0.03;
+const MISSILE_LIFE = 120;
 const MISSILE_COLLISION_RADIUS = 2;
 
 var playerMissiles = [];
@@ -19,41 +19,70 @@ function missileClass() {
 	this.move = function() {
 		if(this.missileLife > 0) {
 			this.missileLife--;
-			this.vX += Math.cos(this.ang) * THRUST_POWER;
-			this.vY += Math.sin(this.ang) * THRUST_POWER;
-			this.pointAtTarget();
+			this.vX += Math.cos(this.ang) * this.thrust;
+			this.vY += Math.sin(this.ang) * this.thrust;
+			if(this.target != undefined) {
+				this.pointAtTarget();
+			}
 			this.superclassMove();
+
+			this.vX *= SPACESPEED_DECAY_MULT;
+			this.vY *= SPACESPEED_DECAY_MULT;
 		} else {
 			this.isDead = true;
 		}
 	}
 
 	this.selectTarget = function(shipFiring) {
+		this.target = undefined;
 		if(shipFiring != p1) {
-			this.target = p1;
+			var missileFacingVector = {x: Math.cos(this.ang), y: Math.sin(this.ang)};
+			var vectorToTarget = {x: p1.x - this.x, y: p1.y - this.y};
+			var angleToFaceTarget = Math.atan2(missileFacingVector.x * vectorToTarget.y - missileFacingVector.y * vectorToTarget.x, missileFacingVector.x * vectorToTarget.x + missileFacingVector.y * vectorToTarget.y);
+			if(angleToFaceTarget >= -Math.PI/3 && angleToFaceTarget <= Math.PI/3) {
+				this.target = p1;
+			}
 		} else {
 			var minDistance;
 			for(var i = 0; i < allUFOs.length; i++) {
-				var distance = distanceBetween(this, ullUFOs[i]);
-				if(minDistance == undefined || distance < minDistance) {
-					this.target = ullUFOs[i];
-					minDistance = distance;
+				var missileFacingVector = {x: Math.cos(this.ang), y: Math.sin(this.ang)};
+				var vectorToTarget = {x: allUFOs[i].x - this.x, y: allUFOs[i].y - this.y};
+				var angleToFaceTarget = Math.atan2(missileFacingVector.x * vectorToTarget.y - missileFacingVector.y * vectorToTarget.x, missileFacingVector.x * vectorToTarget.x + missileFacingVector.y * vectorToTarget.y);
+
+				if(angleToFaceTarget >= -Math.PI/3 && angleToFaceTarget <= Math.PI/3) {
+					var distance = distanceBetween(this, allUFOs[i]);
+					if(minDistance == undefined || distance < minDistance) {
+						this.target = allUFOs[i];
+						minDistance = distance;
+					}
 				}
 			}
 		}
 	}
 
 	this.pointAtTarget = function() {
-
+		var missileFacingVector = {x: Math.cos(this.ang), y: Math.sin(this.ang)};
+		var missileVector = {x: this.vX, y: this.vY};
+		var vectorToTarget = {x: this.target.x - this.x, y: this.target.y - this.y};
+		var angleToTarget = Math.atan2(missileVector.x * vectorToTarget.y - missileVector.y * vectorToTarget.x, missileVector.x * vectorToTarget.x + missileVector.y * vectorToTarget.y);
+		var angleToFaceTarget = Math.atan2(missileFacingVector.x * vectorToTarget.y - missileFacingVector.y * vectorToTarget.x, missileFacingVector.x * vectorToTarget.x + missileFacingVector.y * vectorToTarget.y);
+		if(angleToTarget < -Math.PI/64 && angleToFaceTarget >= -Math.PI/2) {
+			this.ang -= this.turnRate * Math.PI;
+		} else if(angleToTarget > Math.PI/64 && angleToFaceTarget <= Math.PI/2) {
+			this.ang += this.turnRate * Math.PI;
+		}
 	}
 
-	this.shootFrom = function(shipFiring, shotSpeed, missileLife) {
+	this.shootFrom = function(shipFiring, missileThrust, missileTurnRate, missileLife) {
 		this.x = shipFiring.x;
 		this.y = shipFiring.y;
+
+		this.thrust = missileThrust;
+		this.turnRate = missileTurnRate;
 		this.ang = shipFiring.ang;
 
-		this.vX = Math.cos(shipFiring.ang) * shotSpeed + shipFiring.vX;
-		this.vY = Math.sin(shipFiring.ang) * shotSpeed + shipFiring.vY;
+		this.vX = Math.cos(this.ang);
+		this.vY = Math.sin(this.ang);
 
 		this.missileLife = missileLife;
 		this.selectTarget(shipFiring);
