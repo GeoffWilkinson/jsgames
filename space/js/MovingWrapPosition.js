1bc2e1770018c15d872656e38cc47d8c3e7f6895
Unknown <geoff.w.wilkinson@gmail.com>
2017-07-19 03:18:58 +0700

    Fixed bug with elastic collision
    Previously we were using the ship's orientation angle as the angle of velocity for the object, we now find the real angular component of the object's velocity.

--- space/js/MovingWrapPosition.js
+++ space/js/MovingWrapPosition.js
@@ -33,14 +33,45 @@ function movingWrapPositionClass() {
 
 	this.handleElasticObjectCollision = function(otherEntity) {
 		var contactAngle = Math.atan2(otherEntity.y - this.y, otherEntity.x - this.x);
+		var thisVAngle = Math.atan2(this.vY, this.vX);
 		var thisVSpeed = Math.sqrt(this.vX * this.vX + this.vY * this.vY);
+		var otherVAngle = Math.atan2(otherEntity.vY, otherEntity.vX);
 		var otherVSpeed = Math.sqrt(otherEntity.vX * otherEntity.vX + otherEntity.vY * otherEntity.vY);
 
-		this.vX = (thisVSpeed * Math.cos(this.ang - contactAngle) * (this.mass - otherEntity.mass) + (2 * otherEntity.mass * otherVSpeed * Math.cos(otherEntity.ang - contactAngle))) * Math.cos(contactAngle) / (this.mass + otherEntity.mass) + (thisVSpeed * Math.sin(this.ang - contactAngle) * Math.cos(contactAngle + Math.PI/2));
-		this.vY = (thisVSpeed * Math.cos(this.ang - contactAngle) * (this.mass - otherEntity.mass) + (2 * otherEntity.mass * otherVSpeed * Math.cos(otherEntity.ang - contactAngle))) * Math.sin(contactAngle) / (this.mass + otherEntity.mass) + (thisVSpeed * Math.sin(this.ang - contactAngle) * Math.sin(contactAngle + Math.PI/2));
+		this.vX = (thisVSpeed * Math.cos(thisVAngle - contactAngle) * (this.mass - otherEntity.mass) + (2 * otherEntity.mass * otherVSpeed * Math.cos(otherVAngle - contactAngle))) * Math.cos(contactAngle) / (this.mass + otherEntity.mass) + (thisVSpeed * Math.sin(thisVAngle - contactAngle) * Math.cos(contactAngle + Math.PI/2));
+		this.vY = (thisVSpeed * Math.cos(thisVAngle - contactAngle) * (this.mass - otherEntity.mass) + (2 * otherEntity.mass * otherVSpeed * Math.cos(otherVAngle - contactAngle))) * Math.sin(contactAngle) / (this.mass + otherEntity.mass) + (thisVSpeed * Math.sin(thisVAngle - contactAngle) * Math.sin(contactAngle + Math.PI/2));
 
-		otherEntity.vX = (otherVSpeed * Math.cos(otherEntity.ang - contactAngle) * (otherEntity.mass - this.mass) + (2 * this.mass * thisVSpeed * Math.cos(this.ang - contactAngle))) * Math.cos(contactAngle) / (otherEntity.mass + this.mass) + (otherVSpeed * Math.sin(otherEntity.ang - contactAngle) * Math.cos(contactAngle + Math.PI/2));
-		otherEntity.vY = (otherVSpeed * Math.cos(otherEntity.ang - contactAngle) * (otherEntity.mass - this.mass) + (2 * this.mass * thisVSpeed * Math.cos(this.ang - contactAngle))) * Math.sin(contactAngle) / (otherEntity.mass + this.mass) + (otherVSpeed * Math.sin(otherEntity.ang - contactAngle) * Math.sin(contactAngle + Math.PI/2));
+		otherEntity.vX = (otherVSpeed * Math.cos(otherVAngle - contactAngle) * (otherEntity.mass - this.mass) + (2 * this.mass * thisVSpeed * Math.cos(thisVAngle - contactAngle))) * Math.cos(contactAngle) / (otherEntity.mass + this.mass) + (otherVSpeed * Math.sin(otherVAngle - contactAngle) * Math.cos(contactAngle + Math.PI/2));
+		otherEntity.vY = (otherVSpeed * Math.cos(otherVAngle - contactAngle) * (otherEntity.mass - this.mass) + (2 * this.mass * thisVSpeed * Math.cos(thisVAngle - contactAngle))) * Math.sin(contactAngle) / (otherEntity.mass + this.mass) + (otherVSpeed * Math.sin(otherVAngle - contactAngle) * Math.sin(contactAngle + Math.PI/2));
 	}
 
+	this.takeDamage = function(incomingDamage) {
+		var shieldDamage = 0;
+		var hullDamage = 0;
+		if(this.shieldhp < incomingDamage) {
+			shieldDamage = this.shieldhp;
+			hullDamage = incomingDamage - this.shieldhp;
+		} else {
+			shieldDamage = incomingDamage;
+		}
+
+		this.shieldhp -= shieldDamage;
+		this.hp -= hullDamage;
+		if(this.hp <= 0) {
+			this.hp = 0;
+			this.isDead = true;
+		}
+
+		if(shieldDamage > 0) {
+			var shieldDamageTaken = new floatingTextClass();
+			shieldDamageTaken.init(shieldDamage, this.x, this.y, 'blue');
+			allFloatingText.push(shieldDamageTaken);
+		}
+
+		if(hullDamage > 0) {
+			var hullDamageTaken = new floatingTextClass();
+			hullDamageTaken.init(hullDamage, this.x, this.y, 'red');
+			allFloatingText.push(hullDamageTaken);
+		}
+	}
 } // end of class
